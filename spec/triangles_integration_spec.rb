a3c0a0b9b44fcf7c36da13e97c0568df78049a86
andrew accuardi and caitlin ashtari <andrew@>
2016-11-28 10:38:39 -0800

    add passing integration spec and readme.

--- spec/triangles_integration_spec.rb
+++ spec/triangles_integration_spec.rb
@@ -1,10 +1,15 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('', {:type => :feature}) do
-#   it('') do
-#
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the triangle path', {:type => :feature}) do
+  it('take three user inputs and output a type of triangle') do
+    visit('/')
+    fill_in('side1', :with => "4")
+    fill_in('side2', :with => "4")
+    fill_in('side3', :with => "4")
+    click_button('Make Triangle!')
+    expect(page).to have_content("equalateral")
+  end
+end
