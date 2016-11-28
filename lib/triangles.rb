ad7c42b84dd6fda6742a7e0abff12afd4729ce22
andrew accuardi and caitlin ashtari <caitlin@>
2016-11-28 09:33:41 -0800

    add passing triangle spec and isosceles triangle spec.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -6,10 +6,14 @@ class Triangle
   end
 
   define_method(:triangles) do
-    if (@side1 == @side2) && (@side2 == @side3) && (@side3 == @side1)
-      true
-    else
+    if (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
       false
+    else
+      if (@side1 == @side2) && (@side2 == @side3) && (@side3 == @side1)
+        true
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+        true
+      end
     end
   end
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -7,8 +7,23 @@ describe('Triangle#triangles') do
     expect(test_triangle.triangles()).to(eq(false))
   end
 
+  it('returns true if it is a triangle') do
+    test_triangle =  Triangle.new(4, 5, 5)
+    expect(test_triangle.triangles()).to(eq(true))
+  end
+
   it('returns true if it is an equalateral triangle') do
     test_triangle =  Triangle.new(4, 4, 4)
     expect(test_triangle.triangles()).to(eq(true))
+  end
+
+  it('return true if it is an isosceles triangle') do
+    test_triangle =  Triangle.new(5, 5, 4)
+    expect(test_triangle.triangles()).to(eq(true))
+  end
+
+  it('return false if the unequal side is more than twice the two equal sides') do
+    test_triangle = Triangle.new(6, 6, 24)
+    expect(test_triangle.triangles()).to(eq(false))
   end
 end
